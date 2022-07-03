17b517c9f57c1e7948423f109f96701543af0d8f
wanni0928 <chlgudwnd123@gmail.com>
2022-07-03 20:27:52 +0900

    Fix: cache 설정내용 수정

---
+++ src/main/java/me/cache/sample/config/CacheConfig.java
@@ -0,0 +1,49 @@
+package me.cache.sample.config;
+
+import org.ehcache.config.builders.CacheConfigurationBuilder;
+import org.ehcache.config.builders.ExpiryPolicyBuilder;
+import org.ehcache.config.builders.ResourcePoolsBuilder;
+import org.ehcache.config.units.EntryUnit;
+import org.ehcache.config.units.MemoryUnit;
+import org.ehcache.expiry.ExpiryPolicy;
+import org.ehcache.jsr107.Eh107Configuration;
+import org.hibernate.cache.jcache.ConfigSettings;
+import org.springframework.boot.autoconfigure.cache.JCacheManagerCustomizer;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.cache.CacheManager;
+import javax.cache.Caching;
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+  public static final String DB_CACHE = "db_cache";
+
+//  private final javax.cache.configuration.Configuration<Object, Object> jcacheConfiguration;
+
+  @Bean
+  public javax.cache.configuration.Configuration<Object, Object> jcacheConfiguration() {
+    return Eh107Configuration.fromEhcacheCacheConfiguration(CacheConfigurationBuilder.newCacheConfigurationBuilder(Object.class, Object.class,
+            ResourcePoolsBuilder.newResourcePoolsBuilder()
+                    .heap(10000, EntryUnit.ENTRIES))
+            .withSizeOfMaxObjectSize(1000, MemoryUnit.B)
+            .withExpiry(ExpiryPolicyBuilder.timeToIdleExpiration(Duration.ofSeconds(300)))
+            .withExpiry(ExpiryPolicyBuilder.timeToLiveExpiration(Duration.ofSeconds(600))));
+  }
+
+  @Bean
+  public HibernatePropertiesCustomizer hibernatePropertiesCustomizer(javax.cache.CacheManager cacheManager) {
+    return hibernateProperties -> hibernateProperties.put(ConfigSettings.CACHE_MANAGER, cacheManager);
+  }
+
+  @Bean
+  public JCacheManagerCustomizer cacheManagerCustomizer() {
+    return cm -> {
+      cm.createCache(DB_CACHE, jcacheConfiguration());
+    };
+  }
+}
